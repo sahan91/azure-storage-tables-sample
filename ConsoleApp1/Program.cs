ea04d5c18cc549bab9471a92ee46fe6570e11ed4
Sahan Serasinghe <sahan.serasinghe@readify.net>
2019-10-18 14:28:22 +1030

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static CloudTable employees;
+
+        static void Main(string[] args)
+        {
+            // Setup the connection
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("DB Endpoint");
+            
+            // Init the table client
+            CloudTableClient tableClient = cloudStorageAccount.CreateCloudTableClient();
+
+            // Get a handler to the Employees table
+            employees = tableClient.GetTableReference("Employees");
+
+            // Add the data to the table
+            var employeeEntity = new EmployeeEntity("John", "Doe");
+            TableOperation insertop = TableOperation.Insert(employeeEntity);
+
+            try
+            {
+                InsertData(insertop).Wait();
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"There was an exception: {ex.ToString()}");
+            }
+        }
+
+        private static async Task InsertData(TableOperation insertop)
+        {
+            await employees.ExecuteAsync(insertop);
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmployeeEntity : TableEntity
+    {
+        public EmployeeEntity(string firstName, string lastName)
+        {
+            this.PartitionKey = "staff";
+            this.RowKey = firstName + " " + lastName ;
+        }
+    }
+}
